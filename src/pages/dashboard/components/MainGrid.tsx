71d2ad5c1c7bd7e98fa006498a605f75569bf6fc
Matheus Solano Dos Santos Neves <matheussneves@hotmail.com>
2024-11-14 23:30:25 -0300

    ss

--- src/pages/dashboard/components/MainGrid.tsx
+++ src/pages/dashboard/components/MainGrid.tsx
@@ -63,9 +63,7 @@ export default function MainGrid() {
             <StatCard {...card} />
           </Grid>
         ))}
-        <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
-          <HighlightedCard />
-        </Grid>
+        
         <Grid size={{ xs: 12, md: 6 }}>
           <SessionsChart />
         </Grid>
